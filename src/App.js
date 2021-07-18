6fbdb0a2b4fd7024c5c38be9bfbde766cd813343
dvdtsb <david15tache@gmail.com>
2021-07-18 14:31:38 +0300

    aaa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import LeButton from "./LeButton";
+function App() {
+  return (
+    <div>
+      <Header/>
+      <LeButton color="green"/>
+      <LeButton color="red"/>
+      <LeButton color="blue"/>
+      <LeButton color="black"/>
+    </div>
+  );
+}
+
+export default App;
