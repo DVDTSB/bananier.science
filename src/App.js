7fd8ceba0ee487b10e650e1c76afb035d649fdcd
dvdtsb <david15tache@gmail.com>
2021-07-19 10:31:39 +0300

    added epicness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 function App() {
   return (
-    <div>
-      <h1 class="bananier"><i>bananier</i></h1>
+    <>
+    <table>
+      <tr class="navbar"> 
+        <td class="element">aaaa</td>
+        <td class="element">uwu</td>
+        <td class="element">why</td>
+        <td class="element">help</td>
+      </tr>
+    </table>
+    <div class="bananier">
+      <a onClick={()=>{
+        window.open("https://ro.wikipedia.org/wiki/Bananier", "_blank")
+      }}>
+        <h1><i>bananier</i></h1>
+      </a>
     </div>
+    </>
   );
 }
 
