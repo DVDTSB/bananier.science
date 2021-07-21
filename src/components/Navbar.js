e5c3e50ee232bba0dc7e907c4e986f0f3a2be57f
dvdtsb <david15tache@gmail.com>
2021-07-21 13:24:20 +0300

    added pages and shit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
   return (
     <div>
       <tr class="navbar"> 
-        <td class="element"><Link to="/"><h1 class="hoverable">main</h1></Link></td>
-				<td class="element"><Link to="/history"><h1 class="hoverable">history</h1></Link></td>
-				<td class="element"><Link to="/why"><h1 class="hoverable">why</h1></Link></td>
-				<td class="element"><Link to="/uwu"><h1 class="hoverable">uwu</h1></Link></td>
+        <td class="element"><Link to="/"><h2 class="hoverable">main</h2></Link></td>
+				<td class="element"><Link to="/history"><h2 class="hoverable">history</h2></Link></td>
+				<td class="element"><Link to="/why"><h2 class="hoverable">why</h2></Link></td>
+				<td class="element"><Link to="/uwu"><h2 class="hoverable">uwu</h2></Link></td>
     	</tr>
     </div>
   );
