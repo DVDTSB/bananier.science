9aebd57928df766f8df574c5d5767e7e14f594bc
dvdtsb <david15tache@gmail.com>
2021-07-31 21:51:05 +0300

    aa

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Main from "./pages/Main";
 import History from "./pages/History";
 import Why from "./pages/Why";
 import UwU from "./pages/UwU";
+import RickRoll from "./pages/RIckRoll";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
         <Route path="/history" exact component={() => <History />} />
         <Route path="/why" exact component={() => <Why />} />
         <Route path="/uwu" exact component={() => <UwU />} />
+        <Route path="/epic" exact component={() => <RickRoll />} />
       </Switch>
+      
     </Router>
   );
 }
---
+++ src/pages/RIckRoll.js
@@ -0,0 +1,9 @@
+function RickRoll() {
+  return (
+    <>
+    {window.open("https://youtu.be/xvFZjo5PgG0", "_blank")}
+    </>
+  );
+}
+
+export default RickRoll;
