c0fbb761e5689e8833488be2e67227d36d08eb5e
DVDTSB <david15tache@gmail.com>
2022-05-28 12:14:31 +0300

    a

--- src/pages/projects/Xenaf.js
+++ src/pages/projects/Xenaf.js
@@ -1,6 +1,5 @@
 
 import Navbar from "../../components/Navbar";
-import Element from "../../components/Element";
 function Xenaf() {
   return (
     <>
@@ -12,9 +11,9 @@ function Xenaf() {
     All studens of the National College "Andrei Saguna", Romania. <br/>
     Coordonator, Carmen Tanasescu.
     <h2>The ship</h2>
-    <iframe src="https://docs.google.com/viewer?srcid=1gt0PgomDbzd0TluUxG0lU1bZHlNnP03X&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
+    <iframe title="ship" src="https://docs.google.com/viewer?srcid=1gt0PgomDbzd0TluUxG0lU1bZHlNnP03X&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
     <h2>The comic</h2>
-    <iframe src="https://docs.google.com/viewer?srcid=10KAHjpCmdLSZj-x1nnevQnjeSex4nRi2&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
+    <iframe title="comic" src="https://docs.google.com/viewer?srcid=10KAHjpCmdLSZj-x1nnevQnjeSex4nRi2&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
     </div>
     </>
   );
